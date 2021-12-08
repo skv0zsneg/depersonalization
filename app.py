250efe6056c7327bbb6c2798b885bf0044d470af
skvozsneg <1_0_91@bk.ru>
2021-12-09 00:38:25 +0300

    [upd] Update & rename some files. Change .html's.

--- app.py
+++ app.py
@@ -3,11 +3,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/description")
-def description():
-    return render_template('index.html')
 
-@app.route("/test-stand")
-def test_stand():
-    return render_template('test_stand.html')
+@app.route("/")
+@app.route("/start_testing")
+def description():
+    return render_template('testing_page.html')
