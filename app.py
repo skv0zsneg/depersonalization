262ba2d358d7a864947705757e60e60981bca6d8
skvozsneg <1_0_91@bk.ru>
2021-12-23 22:11:19 +0300

    [add|upd] Add new pages.

--- app.py
+++ app.py
@@ -5,6 +5,26 @@ app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/start_testing")
-def description():
-    return render_template('testing_page.html')
+@app.route("/creating-test-db")
+def testing_methods():
+    return render_template('creating_test_db.html')
+
+
+@app.route("/1-method")
+def method_1():
+    return render_template('fst_dp_method.html')
+
+
+@app.route("/2-method")
+def method_2():
+    return render_template('scd_dp_method.html')
+
+
+@app.route("/3-method")
+def method_3():
+    return render_template('thrd_dp_method.html')
+
+
+@app.route("/summary")
+def summary():
+    return render_template('summary.html')
